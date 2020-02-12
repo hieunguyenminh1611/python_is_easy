ce7950d08fe0daff2d61c9440894b06cc880c45f
hieunguyenminh1611 <60960739+hieunguyenminh1611@users.noreply.github.com>
2020-02-12 15:09:55 +0700

    main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+'''This is a about a beautiful song
+which I like most in my graduation days'''
+Artist = "Kishore Kumar"
+#The genre names the song is associated with.
+Genre = "Hindi Classical"
+#The duration of the song in milliseconds.
+DurationInSeconds = 234
+#The name of the album.
+albumname= "O mere sapno ki rani"
+#Composer of the song
+composerName = "Bibhuti Rath"
+contentRating = "****"
+"""The International Standard
+Recording Code (ISRC) for the song.""" 
+isrc= "ABC12321323456"
+#The movement count of this song.
+Movementcount= 6543
+#The movement name of this song.
+movementname = "Loveall"
+#The movement number of this song.
+movementnumber = 342167
+print(Artist)
+print(Genre)
+print(DurationInSeconds)
+print(albumname)
+print(composerName)
+print(contentRating)
+print(isrc)
+print(Movementcount)
+print(movementname)
+print(movementnumber)
